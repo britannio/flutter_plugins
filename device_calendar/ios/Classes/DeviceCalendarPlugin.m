5b639be92cf28d5624ce4d8c29274145132f67ab
britannio <britanniojarrett@gmail.com>
2019-08-21 22:10:49 +0100

    add valid import to DeviceCalendarPlugin.m

--- device_calendar/ios/Classes/DeviceCalendarPlugin.m
+++ device_calendar/ios/Classes/DeviceCalendarPlugin.m
@@ -1,4 +1,5 @@
-#import "DeviceCalendarPlugin.h"
+// #import "DeviceCalendarPlugin.h"
+#import <device_calendar/DeviceCalendarPlugin.h>
 
 @implementation DeviceCalendarPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
